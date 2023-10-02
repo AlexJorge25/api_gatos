749b48424df7693d6536d158a847ae0385944526
Alexandro Jorge <alexandro.jorge@proway.treina>
2023-10-02 14:52:43 -0300

    coloquei o ansyc await no js

--- script.js
+++ script.js
@@ -1,30 +1,16 @@
-// Função para criar e exibir 10 imagens de gatos diferentes
-function trocarImagem() {
-    // Faz uma única requisição para a API "The Cat API" para obter 10 imagens de gatos diferentes
-    fetch(`https://api.thecatapi.com/v1/images/search?limit=10`)
-    .then((dados) => dados.json())
-    .then((response) => {
-            if (response && response.length >= 0) {
-                
-                var container = document.getElementById('container');
-                
-                container.innerHTML = ''
-                
-                for (var i = 0; i < response.length; i++) {
-                    
-                    var imagem = document.createElement("img");
-                    imagem.classList.add("imagem")
-                    
-                    
-                    const imageUrl = response[i].url;
-                    
-                    
-                    imagem.src = imageUrl;
-                    
-                    
-                    container.appendChild(imagem);
-                }
-            }
-        })
-        .catch((erro) => console.log(erro));
+async function trocarImagem() {
+    var dados = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10`)
+    var response = await dados.json()
+    if (response && response.length >= 0) {
+        var container = document.getElementById('container');
+        container.innerHTML = ''
+        for (var i = 0; i < response.length; i++) {
+            var imagem = document.createElement("img");
+            imagem.classList.add("imagem")
+            const imageUrl = response[i].url;
+            imagem.src = imageUrl;
+            container.appendChild(imagem);
+        }
+    }
 }
+
